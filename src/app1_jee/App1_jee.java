c3702ee9c1f319eff0e47958666ddb50cb5fdfe5
armel <chardanmiss@yahoo.fr>
2013-03-04 11:51:50 +0100

    teste de validation1

---
+++ src/app1_jee/App1_jee.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1_jee;
+
+/**
+ *
+ * @author armel
+ */
+public class App1_jee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("*******entity in desktop application *********");
+    }
+}
